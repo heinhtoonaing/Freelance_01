57d056b011c51b9738ddd12037e7d274a9e962cb
Hein Htoo Naing <u6440027@au.edu>
2024-09-29 22:19:04 +0700

    Update Register.jsx

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/register",
+        "https://freelance-01-frontend.vercel.app/register",
         { name, phone, email, role, password },
         {
           headers: {
