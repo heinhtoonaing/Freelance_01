7337a82be6da61968183b93518c4825fdee261ee
Hein Htoo Naing <u6440027@au.edu>
2024-09-29 22:06:49 +0700

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,6 +5,8 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true, // Set httpOnly to true
+    secure: process.env.NODE_ENV === "production", // cookie sent only over HTTPS
+    sameSite: "none", // allows cookies to be sent cross-origin (important for Vercel)
   };
 
   res.status(statusCode).cookie("token", token, options).json({
